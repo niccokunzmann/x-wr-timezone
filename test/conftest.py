cf7b42cadae995fbeae50d107f96c8be75f325f8
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-01-11 23:23:14 +0000

    first test failing

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,8 @@
 """Test and fixture initialization."""
 import icalendar
 import pytest
+import sys
+import os
 
 HERE = os.path.dirname(__file__)
 REPO = os.path.dirname(HERE)
@@ -9,10 +11,10 @@ sys.path.append(REPO)
 
 CALENDARS_FOLDER = os.path.join(HERE, "calendars")
 
-ID_END_INPUT = "-in"
-ID_END_OUTPUT = "-out"
+NAME_END_INPUT = ".in"
+NAME_END_OUTPUT = ".out"
 
-calendars = {} # id: calendar
+calendars = {} # name: calendar
 
 class TestCalendar:
     """A calendar in the test folder."""
@@ -21,40 +23,48 @@ class TestCalendar:
         self.path = path
 
     @property
-    def id(self):
+    def name(self):
         """Return the nicely readable id of the calendar."""
         name = os.path.basename(self.path)
         name = os.path.splitext(name)[0]
-        return name
+        return name.replace("-", " ")
 
     def is_input(self):
         """Whether this is a calendar which is used as an input for to_standard()."""
-       return self.id.endswith(ID_END_INPUT)
+        return self.name.endswith(NAME_END_INPUT)
 
-    def get_corrected_output_id(self):
+    def get_corrected_output_name(self):
         assert self.is_input(), "Only input calendars can have an output."
-        return self.id[:len(self.id) - len(ID_END_INPUT)] + ID_END_OUTPUT
+        return self.name[:len(self.name) - len(NAME_END_INPUT)] + NAME_END_OUTPUT
 
     def has_corrected_output(self):
         """Whether this has a calendar resulting from to_standard()."""
-        return self.get_corrected_output_id() in calendars
+        return self.get_corrected_output_name() in calendars
 
     def get_corrected_output(self):
         """Return the corrected output of to_standard() for this calendar.
 
         KeyError if absent."""
-        return calendars[self.get_corrected_output_id()]
+        return calendars[self.get_corrected_output_name()]
 
+    def __repr__(self):
+        return "<{}>".format(self.id)
 
 for calendar_file in os.listdir(CALENDARS_FOLDER):
     calendar_path = os.path.join(CALENDARS_FOLDER, calendar_file)
     calendar = TestCalendar(calendar_path)
-    calendars[calendar.id] = calendar
+    calendars[calendar.name] = calendar
 
 # for parametrizing fixtures, see https://docs.pytest.org/en/latest/fixture.html#parametrizing-fixtures
 @pytest.fixture(params=[calendar for calendar in calendars.values() if calendar.is_input() and calendar.has_corrected_output()])
 def calendar_pair(request):
     return CaledarPair(request.param(), request.param().get_corrected_output())
+
+@pytest.fixture()
+def to_standard():
+    """Skip a test because it needs to be written first."""
+    import x_wr_timezone
+    return x_wr_timezone.to_standard
 
 @pytest.fixture()
 def todo():
