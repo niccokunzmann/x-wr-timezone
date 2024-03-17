473d4e911cae372bd7346ea5ec1c5dc86f4f153f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-03-17 05:10:42 +0000

    v0.0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ PACKAGE_NAME = "x_wr_timezone"
 HERE = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, HERE)  # for package import
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = 'Nicco Kunzmann'
 
 
